5e95df8764fc7889d11039f851e04aef5913ea18
Jinank Jain <jinank94@gmail.com>
2018-07-02 11:12:58 +0200

    refactor client code

--- go/lib/apnad/crypto.go
+++ go/lib/apnad/crypto.go
@@ -10,6 +10,7 @@ func GenKeyPairs() (common.RawBytes, common.RawBytes, error) {
 	return crypto.GenKeyPairs(crypto.Curve25519xSalsa20Poly1305)
 }
 
+// GenSharedSecret returns shared secret for diffie-hellman kex
 func GenSharedSecret(pubkey, privkey common.RawBytes) (common.RawBytes, error) {
 	return crypto.GenSharedSecret(pubkey, privkey, crypto.Curve25519xSalsa20Poly1305)
 }
